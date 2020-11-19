4a84ba32ed02ce552afe3dd7c5d6fb9be782ca81
Dmytro Batarin <dmytro.jr@batarin.zp.ua>
2020-11-19 22:34:16 +0200

    Create user

--- src/users/user.resolver.ts
+++ src/users/user.resolver.ts
@@ -1,4 +1,8 @@
-import { Query, Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import {
+  CreateAccountOutput,
+  CreateAccountInput,
+} from './dtos/create-account.dto';
 import { User } from './entities/users.entity';
 import { UsersService } from './user.service';
 
@@ -9,5 +13,26 @@ export class UsersResolver {
   @Query((returns) => Boolean)
   hi() {
     return true;
+  }
+
+  @Mutation((returns) => CreateAccountOutput)
+  async createAccount(
+    @Args('input') createAccountInput: CreateAccountInput,
+  ): Promise<CreateAccountOutput> {
+    try {
+      const [ok, error] = await this.usersService.createAccount(
+        createAccountInput,
+      );
+
+      return {
+        ok,
+        error,
+      };
+    } catch (error) {
+      return {
+        ok: false,
+        error,
+      };
+    }
   }
 }
--- src/users/user.service.ts
+++ src/users/user.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import { CreateAccountInput } from './dtos/create-account.dto';
 import { User } from './entities/users.entity';
 
 @Injectable()
@@ -8,4 +9,27 @@ export class UsersService {
   constructor(
     @InjectRepository(User) private readonly users: Repository<User>,
   ) {}
+
+  async createAccount({
+    email,
+    password,
+    role,
+  }: CreateAccountInput): Promise<[Boolean, String?]> {
+    // check new user
+    // create user & hash password
+
+    try {
+      const exists = await this.users.findOne({ email });
+
+      if (exists) {
+        return [false, 'There is a user with that email already'];
+      }
+
+      await this.users.save(this.users.create({ email, password, role }));
+
+      return [true];
+    } catch (e) {
+      return [false, "Couldn't create account"];
+    }
+  }
 }
