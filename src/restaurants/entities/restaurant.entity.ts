058ce0e79421393eb7405be850024d955673fa90
Dmytro Batarin <dmytro.jr@batarin.zp.ua>
2020-11-19 21:16:39 +0200

    Created first table

--- src/restaurants/entities/restaurant.entity.ts
+++ src/restaurants/entities/restaurant.entity.ts
@@ -1,16 +1,26 @@
 import { Field, ObjectType } from '@nestjs/graphql';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @ObjectType()
+@Entity()
 export class Restaurant {
+  @Field((type) => Number)
+  @PrimaryGeneratedColumn()
+  id: Number;
+
   @Field((type) => String)
+  @Column()
   name: String;
 
   @Field((type) => Boolean, { nullable: true })
+  @Column()
   isVegan?: Boolean;
 
   @Field((type) => String)
+  @Column()
   address: String;
 
   @Field((type) => String)
+  @Column()
   ownerName: String;
 }
